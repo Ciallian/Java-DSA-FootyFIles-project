f403c5adcc00a10c2ad7371bc3614f712c71ea48
Ciallian <ciallianbain@gmail.com>
2026-03-09 14:47:39 +0000

    Fiixed parameters

--- Main.java
+++ Main.java
@@ -1,17 +1,8 @@
 public class Main {
-    record SoccerPlayer(int age, double rating, String name, String nationalTeam, String club, String position, double value, double wage) {}
+    record SoccerPlayer(int age, double overall, double potential, String name, String nationalTeam, String club, String position, long value, long wage, int contractStart, int contractEnd) {}
 
     public static void main(String[] args) {
-        SoccerPlayer player1 = new SoccerPlayer(12, 8.7, "Rory", "Zimbabwe", "Shamrock Rover's", "BAck", 10, 0.02);
-       
-        System.out.println(player1.age);
-        System.out.println(player1.rating);
-        System.out.println(player1.name);
-        System.out.println(player1.nationalTeam);
-        System.out.println(player1.club);
-        System.out.println(player1.position);
-        System.out.println(player1.value);
-        System.out.println(player1.wage);
+        SoccerPlayer player1 = new SoccerPlayer(12, 8.7, 7.0, "Rory", "Zimbabwe", "Shamrock Rover's", "BAck", 10, 2, 02, 11);
     }
 
 }
